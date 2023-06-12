4a2305ef8f63657b6dae5c8a307ff17dce1cdf0c
LinET1117 <yayash410047@gmail.com>
2023-06-12 22:32:18 +0800

    初始 Spring Boot project

---
+++ src/main/java/com/linet/shoppingmallusingspringboot/ShoppingMallUsingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.linet.shoppingmallusingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallUsingSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallUsingSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linet/shoppingmallusingspringboot/ShoppingMallUsingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linet.shoppingmallusingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallUsingSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
