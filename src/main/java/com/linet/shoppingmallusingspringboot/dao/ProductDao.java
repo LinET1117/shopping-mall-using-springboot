7e043e4cc2ad0d0794378e6a32cc0a393e47f9be
LinET1117 <yayash410047@gmail.com>
2023-06-13 17:09:44 +0800

    商品功能實作-新增商品功能

--- src/main/java/com/linet/shoppingmallusingspringboot/dao/ProductDao.java
+++ src/main/java/com/linet/shoppingmallusingspringboot/dao/ProductDao.java
@@ -1,9 +1,13 @@
 package com.linet.shoppingmallusingspringboot.dao;
 
+import com.linet.shoppingmallusingspringboot.dto.ProductRequest;
 import com.linet.shoppingmallusingspringboot.model. Product;
 
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest) ;
+
 }
--- src/main/java/com/linet/shoppingmallusingspringboot/service/ProductService.java
+++ src/main/java/com/linet/shoppingmallusingspringboot/service/ProductService.java
@@ -1,8 +1,12 @@
 package com.linet.shoppingmallusingspringboot.service;
 
+import com.linet.shoppingmallusingspringboot.dto.ProductRequest;
 import com.linet.shoppingmallusingspringboot.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest) ;
+
 }
