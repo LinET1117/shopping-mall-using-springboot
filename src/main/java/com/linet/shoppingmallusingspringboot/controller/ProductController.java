b4144fb3a5caab15b78a1c0dc14663d76a2c1b5e
LinET1117 <yayash410047@gmail.com>
2023-06-17 18:00:58 +0800

    商品功能實作-新增分頁功能

--- src/main/java/com/linet/shoppingmallusingspringboot/controller/ProductController.java
+++ src/main/java/com/linet/shoppingmallusingspringboot/controller/ProductController.java
@@ -6,13 +6,17 @@ import com.linet.shoppingmallusingspringboot.dto.ProductRequest;
 import com.linet.shoppingmallusingspringboot.model.Product;
 import com.linet.shoppingmallusingspringboot. service. ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org. springframework. beans. factory.annotation. Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
@@ -22,14 +26,20 @@ public class ProductController {
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(@RequestParam(required = false) ProductCategory category,
                                                      @RequestParam(required = false) String search,
+
                                                      @RequestParam(defaultValue = "created_date") String orderBy,
-                                                     @RequestParam(defaultValue = "desc")String sort)
+                                                     @RequestParam(defaultValue = "desc")String sort,
+
+                                                     @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+                                                     @RequestParam(defaultValue = "0") @Min(0)Integer offset)
     {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category) ;
         productQueryParams.setSearch(search) ;
         productQueryParams.setOrderBy(orderBy) ;
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
